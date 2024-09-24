175940260339e95dca90010d1c5f019a03d9e550
ToAN7 <91736051+ToAN7@users.noreply.github.com>
2024-09-24 21:20:52 +0700

    do some mini project based

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 window.addEventListener("load", () => {
 	let header = document.querySelector("h1");
+	let body = document.querySelector("body");
+	let start = Date.now();
 
-	header = document.addEventListener("click", () => {
-		console.log(567342189);
+	header.addEventListener("click", () => {
+		let current = Date.now();
+		console.log(current - start);
+		start = current;
 	});
+
+	let imgs = document.querySelectorAll(".imgs>div");
+
+	for (let i of imgs) {
+		i.addEventListener("click", () => {
+			for (let a of imgs) {
+				a.classList.remove("active");
+			}
+			i.classList.add("active");
+		});
+	}
 });
